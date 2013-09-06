842c9b87e2ea86048b41471b19730d76c031e4cb
orff <michaelhill@gmail.com>
2013-09-06 15:52:15 -0400

    Initial Commit

---
+++ HotTubFeedMachine/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  HotTubFeedMachine
+//
+//  Created by Hill, Michael on 9/6/13.
+//  Copyright (c) 2013 Hill, Michael. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ HotTubFeedMachine/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  HotTubFeedMachine
+//
+//  Created by Hill, Michael on 9/6/13.
+//  Copyright (c) 2013 Hill, Michael. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
